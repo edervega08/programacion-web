c89e456340db167c7df63ea4aaa7e4403336710c
edervega08 <edervb2000@gmail.com>
2026-02-24 09:27:10 -0600

    2

--- ejemplo2.js
+++ ejemplo2.js
@@ -14,3 +14,18 @@ class rectangulo{
         return (2 * this.base) + (2 * this.altura);
     }
 }
+
+triangulo  ={base: 10,
+    altura: 20,
+    nombre: "triangulo",
+    setBase(b){
+        this.base = b;
+    },
+    area(){
+        return (this.base * this.altura) / 2;
+    },
+    toString(){
+        return JSON.stringify(this);    }
+
+   
+}
